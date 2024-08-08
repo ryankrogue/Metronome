539b05df51c4f387c7b6f35378cc5872ab641b88
ryankrogue <ryan.krogue@gmail.com>
2024-08-08 16:28:32 -0600

    Initial Commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by Ryan Krogue on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Ryan Krogue on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
